8248d140ec601d391af814c73ad7be4893e34f98
office <mashuaigood@126.com>
2019-06-05 16:52:28 +0800

    6/5

--- kalman_gain.m
+++ kalman_gain.m
@@ -18,9 +18,10 @@ P_noise = [0.164,0.164,0.15,0.15]*0.01;
 T_noise = [0.23,0.23,0.097,0.097]*0.01;
 y_svar = [N_noise,P_noise,T_noise];
 y_noise = y_svar.*y_design;
-R = diag(y_noise.^2);
+% R = diag(y_noise.^2);
+R = 1*eye(size(C_k,1));
 %% system noise
-Q =  0.002*eye(size(A_k,1));
+Q =  0.002^2*eye(size(A_k,1));
 Q_ = 0.002*eye(size(A,1));
 %% kalman gain
 for k = 1:size(A_k,3)
